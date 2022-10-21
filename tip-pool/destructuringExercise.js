8b6ab9176871fc4baab71737ef114bfa198183d3
tofuhead2015 <tofuhead@gmail.com>
2022-10-21 08:46:24 -0400

    destructuring

---
+++ tip-pool/destructuringExercise.js
@@ -0,0 +1,36 @@
+// numPlanets = 8
+// yearNeptuneDiscovered = 1846
+
+// discoveryYears = {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+// getUserData - returns "Your name is Alejandro and you like purple"
+// getUserData - returns "Your name is Melissa and you like green"
+// getUserData - returns "You name is and you like green"
+
+// Array Destructuring 1
+// first = "Maya
+// second = "Marisa"
+// third = "Chi"
+
+// Array Destructuring 2
+// raindrops = "Raindrops on roses"
+// whiskers = "whiskers on kittens"
+// aFewOfMyFavoriteThings = ["Bright copper kettles", "warm woolen mittens","Brown paper packages tied up with strings"]
+
+// Array Destructuring 3
+// numbers = [10, 30, 20]
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+let {a, b} = obj.numbers
+
+// ES5 Array Swap
+var arr = [1, 2];
+arr[num[0], num[1]] = arr[num[1], num[0]]
+
+raceResults = ([first, second, third, ...rest])=>{first, second, third, rest}
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
